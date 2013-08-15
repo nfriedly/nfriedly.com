ce584d2caf9b9a6002321174d2ee8e6b202a9a6f
Nathan Friedly <nathan@nfriedly.com>
2013-08-15 12:21:52 -0400

    added blog previews & tweaked layout

--- docpad.js
+++ docpad.js
@@ -1,14 +1,26 @@
-# DocPad Configuration File
-# http://docpad.org/docs/config
+// DocPad Configuration File
+// http://docpad.org/docs/config
 
-# Define the DocPad Configuration
-docpadConfig = {
-	collections:
-		techblog: -> @getCollection("html").findAllLive({relativeOutDirPath:'techblog'},[date:-1])
-	templateData:
-		getPreview: (post) -> 
-			post.body.match(/<img [^>]+>/)[0]
-}
+var reImages = /<img [^>]+>/g;
+var reHW = /(height|width)="\d+"/g;
 
-# Export the DocPad Configuration
-module.exports = docpadConfig
+module.exports = {
+	collections: {
+		techblog: function() { 
+			return this.getCollection("html").findAllLive({relativeOutDirPath:'techblog'}, [{date:-1}]);
+		}
+	},
+	templateData: {
+		getFirstImage: function(post) { 
+			var images = post.body.match(reImages);
+			if (!images) return "";
+			var img = images[0];
+			return img.replace(reHW, '');
+		},
+		getPreview: function(post) {
+			var sections = post.body.split('<!--more-->');
+			if (sections.length != 2) return "";
+			return sections[0].replace(reImages, '');
+		},
+	}
+};
