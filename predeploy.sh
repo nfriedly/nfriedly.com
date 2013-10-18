fbe614c6d876d715e7e90a191b625d0196f00a93
Nathan Friedly <nathan@nfriedly.com>
2013-10-18 16:24:15 -0500

    updating autodeploy process with new (now hidden) key

--- predeploy.sh
+++ predeploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # based on https://gist.github.com/bewest/6100033
+# travis encrypt --add -r user-org-name/repo-name 'GIT_NAME="Your Committer Name [via travis key]" GIT_EMAIL=committer@example.com GH_TOKEN=ahead0fxxxxxxxxxxxxxxxxxxx'
+
 
 echo "BUMP HELLO WORLD set up $GH_REPO [via travis] for $GIT_NAME <${GIT_EMAIL}>"
 export REPO_URL="https://$GH_TOKEN@github.com/$GH_REPO.git"
