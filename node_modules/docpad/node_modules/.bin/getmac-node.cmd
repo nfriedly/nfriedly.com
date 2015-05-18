bb58911b0e94eff967e33e065b72357db30fea22
Nathan Friedly <nathan@nfriedly.com>
2015-05-18 09:49:25 -0400

    updating dependencies, removing node_modules folder

--- node_modules/docpad/node_modules/.bin/getmac-node.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\..\getmac\bin\getmac-node" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\..\getmac\bin\getmac-node" %*
-)
