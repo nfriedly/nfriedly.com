3159917efccc0d39bfed73b315ba2b99d1ea8aaf
Nathan Friedly <nathan@nfriedly.com>
2024-07-14 11:27:39 -0400

    portfolio looks about right, but still needs a bit of work - it's currently one huge page, so it takes a while to load even from a local server - I don't have thumbnails working yet, so images are sometimes the wrong shape and also contribute to the poor loading speeds

--- .eleventy.js
+++ .eleventy.js
@@ -21,12 +21,17 @@ module.exports = function (eleventyConfig) {
 		// get unsorted items
 		return collectionApi.getFilteredByTag("techblog").filter(p => p.data.featured).reverse().slice(0,6);
 	});
-
-	eleventyConfig.addCollection("projects", function (collectionApi) {
+ 
+	function projectsCollection(collectionApi) {
 		// get everything from the projects folder, without changing it's tags
 		return collectionApi.getFilteredByGlob("src/projects/*.md");
-	});
+	}
 
+	eleventyConfig.addCollection("projects", projectsCollection);
+
+	eleventyConfig.addCollection("projectsSorted", function(collectionApi) {
+		return projectsCollection(collectionApi).sort((a,b) => a.page.inputPath.localeCompare(b.page.inputPath))
+	})
 
 	eleventyConfig.addShortcode("getFirstImage", function(post) {
 		return post?.content?.match(reImages)?.pop()?.replace(reHW, "").replace(reClass, "");
@@ -61,15 +66,38 @@ module.exports = function (eleventyConfig) {
 		return posts.slice().reverse().find(post => post.data.headerImage)?.data.headerImage;
 	})
 
-	eleventyConfig.addShortcode("normalizeTag", (tag) => {
+	function normalizeTag(tag) {
 		return tag.toLowerCase().replace('++', 'pp').replace('#', '-sharp').replace(/[^a-z0-9]/g, " ").trim().replace(RegExp(" +", "g"), "-");
+	}
+
+	eleventyConfig.addShortcode("normalizeTag", normalizeTag)
+
+	eleventyConfig.addShortcode("projectBadges", (project) => {
+		const badges = [];
+		if (project.data.github || (project.data.link && project.data.link.includes('github.com'))) {
+			let github = project.data.github || project.data.link;
+			if (github.substr(0,4) !== 'http') {
+				github = "https://github.com/nfriedly/" + github;
+			}
+			badges.push(`<a href="${ github }"><i class="fa fa-github"></i></a>`)
+		}
+		if (project.data.npm) {
+			badges.push(`<a href="https://npmjs.org/package/${ project.data.npm  }" class="project-badge"><img src="https://badge.fury.io/js/${ project.data.npm  }.svg" alt="${ project.data.title  } on NPM"></a>`);
+			badges.push(`<a href="https://npmjs.org/package/${ project.data.npm  }" class="project-badge"><img src="https://img.shields.io/npm/dm/${ project.data.npm  }.svg" alt="${ project.data.title  } downloads"></a>`);
+		}
+		if (project.data.bower) {
+			badges.push(`<img src="https://badge.fury.io/bo/${ project.data.bower  }.svg" alt="${ project.data.title  } on Bower">`);
+		}
+		// travis ci badges used to go here, but they stopped working
+		// todo: add GH actions CI badge? maybe automatically detect them!?
+		return badges.join('\n')
 	})
 
-	// eleventyConfig.addShortcode("linkTags", (tags) => {
-	// 	return (tags || []).map(function(tag) {
-	// 		return "<a href=\"#" + normalizeTag(tag) + "\">" + tag + "</a>";
-	// 	  }).join(", ");
-	// })
+	eleventyConfig.addShortcode("linkTags", (tags) => {
+		return (tags?.data?.tags || tags || []).map(function(tag) {
+			return "<a href=\"#" + normalizeTag(tag) + "\">" + tag + "</a>";
+		  }).join(", ");
+	})
 
 	return {
 		dir: {
