65c1e07297b26045883a92b368311a464aaee2ed
Nathan Friedly <nathan@nfriedly.com>
2024-07-14 11:27:39 -0400

    portfolio looks about right, but still needs a bit of work - it's currently one huge page, so it takes a while to load even from a local server - I don't have thumbnails working yet, so images are sometimes the wrong shape and also contribute to the poor loading speeds

--- src/scripts/portfolio.js
+++ src/scripts/portfolio.js
@@ -5,35 +5,7 @@ $(function() {
     var $projects = $('div.projects > article');
     var $tagCloud = $('.tag-cloud a');
 
-    /* defer loading of everything after the first 20 or so projects */
-
-    var loadMore = _.once(function() {
-        // loosely based on https://github.com/defunkt/jquery-pjax
-        $.get('2/', function(data) {
-            var $body = $($.parseHTML(data.match(/<body[^>]*>([\s\S.]*)<\/body>/i)[0]));
-            var $moreProjects = $body.find('div.projects article');
-            $moreContainer.replaceWith($moreProjects);
-            $projects = $projects.add($moreProjects);
-            applyFilter(); // show/hide projects as necessary
-        });
-        $moreContainer.find('p.link').hide();
-        $moreContainer.find('p.loading').fadeIn();
-    });
-
-    // on small-screen devices, wait for the user to click before loading additional
-    $moreContainer.find('a').click(function(e) {
-        loadMore();
-        e.preventDefault();
-    });
-
-    // on large screen devices, automatically load in the rest after the first batch completes
-    $win = $(window);
-    if ($win.width() >= 768) {
-        $win.load(loadMore);
-    }
-
-
-    /* Filter by tags */
+     /* Filter by tags */
 
     function getFilter() {
         return location.hash;
@@ -50,7 +22,6 @@ $(function() {
             var filter = getFilter();
             $projects.not(':has(p.meta a[href=' + filter + '])').addClass('filtered');
             $tagCloud.filter('a[href=' + filter + ']').addClass('active');
-            loadMore(); // if a tag is set, automatically load all remaining projects regardless of browser size and loading status
         }
     }
 
